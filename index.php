d2028e470a0b78e4c890bcda807e554b72748d68
cityji <82261530+cityji@users.noreply.github.com>
2021-11-23 09:03:13 +0530

    add array  variables

--- index.php
+++ index.php
@@ -12,19 +12,32 @@
     </style>
 </head>
 <body>
+    <!-- chap 1 : intro -->
     <h1>This is
     <?php
 echo "<b> PHP </b>";
     ?>
     Store !</h1>
 
+    <!-- chap 2 : Varriables | declaration and usage -->
 <?php
 $name ="iPhone"
 $value = 10000;
 
-echo "<h4>Product is  : .$name. <br> Price  is : .$price.</h4>";
+echo "<h4>Product is  : ".$name." <br> Price  is : ".$price."</h4>";
 
 ?>
 
+    <!-- chap 3 : Array Varriables | Declaration and usage -->
+
+    <?php
+$iPhone['name'] =  "iPhone 13"
+$iPhone['price'] = "$ 10000"
+$iPhone['class'] = "Flagship"; 
+
+echo "Product Name  :".$iphone[name]"<br>";
+echo "Product Price :".$iphone[price]"<br>";
+echo "Product Class :".$iphone[class]"<br>";
+    ?>
 </body>
 </html>
